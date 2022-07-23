80b4197fadeedca6db87f639179c74cebf0434cb
Luka <l.momcilovic61@gmail.com>
2022-07-23 12:25:10 +0200

    Update server URL

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -30,7 +30,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://peygr.herokuapp.com/';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
